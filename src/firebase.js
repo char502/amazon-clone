16657a152f308d1ab92c2496b1e85f693f02b2fb
char502 <char502@hotmail.com>
2020-09-09 21:15:28 +0100

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDO_VmfQ4yVbxBcJwUhDtPyQtKKYugiqJM",
+  authDomain: "clone-14d82.firebaseapp.com",
+  databaseURL: "https://clone-14d82.firebaseio.com",
+  projectId: "clone-14d82",
+  storageBucket: "clone-14d82.appspot.com",
+  messagingSenderId: "806675085986",
+  appId: "1:806675085986:web:995305c935347ec99175fb",
+  measurementId: "G-CG194RPY2P",
+};
