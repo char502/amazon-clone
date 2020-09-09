15da1097bb9ac6e2d5e33fe4fe44b2a0c46177a5
char502 <char502@hotmail.com>
2020-09-09 22:09:55 +0100

    Creating checkout page

---
+++ src/Checkout.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Checkout.css";
+
+function Checkout() {
+  return (
+    <div className="" checkout>
+      <p>This is the Checkout Component</p>
+    </div>
+  );
+}
+
+export default Checkout;
--- src/Product.js
+++ src/Product.js
@@ -1,24 +1,29 @@
 import React from "react";
 import "./Product.css";
 
-function Product(props) {
+import StarIcon from "@material-ui/icons/Star";
+
+function Product({ id, title, image, price, rating }) {
   return (
     <div className="product">
       <div className="product__info">
-        <p>The lean startup</p>
+        <p>{title}</p>
         <p className="product__price">
           <small>$</small>
-          <strong>19.99</strong>
+          <strong>{price}</strong>
         </p>
         <div className="product__rating">
-          <p>🌟</p>
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>
+                <StarIcon />
+              </p>
+            ))}
         </div>
       </div>
 
-      <img
-        src="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
-        alt=""
-      />
+      <img src={image} alt="" />
       <button>Add to Basket</button>
     </div>
   );
